ea984fb03ebda82ba555dcf94f20439d14a5ca71
hailin <hs@topo.cc>
2023-08-23 16:00:20 +0800

    Add topo logo

--- InvoiceGenerator/Program.cs
+++ InvoiceGenerator/Program.cs
@@ -85,8 +85,8 @@ file class InvoiceDocument : IDocument
                 });
             });
 
-            // TODO: Find a good image
-            //row.ConstantItem(100).Height(50).Placeholder();
+            var image = Image.FromFile("Topo-Logo.png");
+            row.ConstantItem(100).Height(50).Image(image);
         });
     }
 
