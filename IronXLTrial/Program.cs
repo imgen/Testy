f6f554ecf5b28d2d9760cde9d9d628748554c517
hailin <hs@topo.cc>
2023-10-13 09:30:54 +0800

    Update Program.cs

--- IronXLTrial/Program.cs
+++ IronXLTrial/Program.cs
@@ -1,13 +1,20 @@
 ﻿// See https://aka.ms/new-console-template for more information
+
+using System.Diagnostics;
+using System.Globalization;
 using IronXL;
 
-IronXL.License.LicenseKey = "IRONSUITE.HS.TOPO.CC.7939-D602816371-A2C3AACCEBDBX7I6-EJCPHVYJRYUB-HUUWDDTJLIQP-J6NFTDAYOARM-S63MFWW22PKM-PENZXCKAQDEX-M3R4WJ-T4BRFMOBNECKUA-DEPLOYMENT.TRIAL-6RRGIE.TRIAL.EXPIRES.14.SEP.2023";
+License.LicenseKey = "IRONSUITE.DEVTEAM.IRO231009.4993.48123-898138E282-ACRME4DB56NUSYLB-YJSNGBKKMZ4T-KY4HHUUTIWLT-3SFX7HUJHUXA-NVGCYP27FM5S-U24W2ZW235DN-BDH5YS-L7EXDTQRTASUUA-IRONSUITE.PROFESSIONAL.SAAS.OEM.5YR-7MWTO7.RENEW.SUPPORT.07.OCT.2028";
 
 var xlsWorkbook = WorkBook.Create(ExcelFileFormat.XLSX);
 xlsWorkbook.Metadata.Author = "IronXL";
 var xlsSheet = xlsWorkbook.CreateWorkSheet("new_sheet");
-xlsSheet["A1"].Value = "Hello World";
-xlsSheet["A2"].Style.BottomBorder.SetColor("#ff6600");
-xlsSheet["A2"].Style.BottomBorder.Type = IronXL.Styles.BorderType.Double;
-var image = xlsSheet.InsertImage("Beauty.jpg", 1, 1, 2, 2);
-xlsWorkbook.SaveAs("NewExcelFile.xlsx"); //Save the excel file
+if (DateOnly.TryParse("2022/02/02", CultureInfo.InvariantCulture,  out var date))
+{
+    xlsSheet["A1"].Value = date.ToDateTime(TimeOnly.MinValue);
+    xlsSheet["A1"].FormatString = "yyyy-MM-dd";
+}
+
+var excelFilePath = Path.Combine(Environment.CurrentDirectory, "NewExcelFile.xlsx");
+xlsWorkbook.SaveAs(excelFilePath); //Save the excel file
+Process.Start("explorer.exe", excelFilePath);
