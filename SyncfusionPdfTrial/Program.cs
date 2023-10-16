65c498caad4ee2a81ba2e8b247da25522e1f944a
hailin <hs@topo.cc>
2023-10-16 12:43:43 +0800

    Try Syncfusion

--- SyncfusionPdfTrial/Program.cs
+++ SyncfusionPdfTrial/Program.cs
@@ -1,5 +1,6 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using System.Diagnostics;
 using Syncfusion.Licensing;
 using Syncfusion.Pdf.Parsing;
 using Syncfusion.Pdf.Security;
@@ -9,9 +10,14 @@ var fileStream = new FileStream("Invoice.pdf", FileMode.Open, FileAccess.Read);
 var pdfDoc = new PdfLoadedDocument(fileStream);
 var pfxFileStream = new FileStream("AATL2.pfx", FileMode.Open);
 var pdfCertificate = new PdfCertificate(pfxFileStream, "ja3Aq*sf9XnGyrseoDtRk");
-var signature = new PdfSignature(pdfDoc, pdfDoc.Pages[0], pdfCertificate, "Topo Solutions Limited");
-pdfDoc.Save(new FileStream(
-    Path.Combine(Environment.CurrentDirectory, "Invoice_signed.pdf"),
-    FileMode.Create
-));
+#pragma warning disable S1481
+var pdfSignature = new PdfSignature(pdfDoc, pdfDoc.Pages[0], pdfCertificate, "Topo Solutions Limited")
+#pragma warning restore S1481
+{
+        TimeStampServer = new TimeStampServer(new Uri("http://aatl-timestamp.globalsign.com/tsa/v4v5effk07zor410rew22z"))
+    };
+
+var signedPdfFilePath = Path.Combine(Environment.CurrentDirectory, "Invoice_signed.pdf");
+pdfDoc.Save(new FileStream(signedPdfFilePath, FileMode.Create));
+Process.Start("explorer.exe", signedPdfFilePath);
 
