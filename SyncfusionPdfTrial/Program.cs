d50efec483a0091fed7ad998bf022e7d8051a498
hailin <hs@topo.cc>
2023-10-19 15:29:10 +0800

    Update license

--- SyncfusionPdfTrial/Program.cs
+++ SyncfusionPdfTrial/Program.cs
@@ -1,11 +1,9 @@
-﻿// See https://aka.ms/new-console-template for more information
-
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using Syncfusion.Licensing;
 using Syncfusion.Pdf.Parsing;
 using Syncfusion.Pdf.Security;
 
-SyncfusionLicenseProvider.RegisterLicense("NRAiBiAaIQQuGjN/V0R+Xk9NfV5AQmBIYVp/TGpJfl96cVxMZVVBJAtUQF1hSn5bd0BjWHtZcHRRRmFV");
+SyncfusionLicenseProvider.RegisterLicense("Ngo9BigBOggjHTQxAR8/V1NHaF5cWWJCf1FpRmJGdld5fUVHYVZUTXxaS00DNHVRdkdgWH9feXRSRGZdV0R3X0o=");
 var fileStream = new FileStream("Invoice.pdf", FileMode.Open, FileAccess.Read);
 var pdfDoc = new PdfLoadedDocument(fileStream);
 var pfxFileStream = new FileStream("AATL2.pfx", FileMode.Open);
@@ -15,7 +13,7 @@ var pdfSignature = new PdfSignature(pdfDoc, pdfDoc.Pages[0], pdfCertificate, "To
 #pragma warning restore S1481
 {
         TimeStampServer = new TimeStampServer(new Uri("http://aatl-timestamp.globalsign.com/tsa/v4v5effk07zor410rew22z"))
-    };
+};
 
 var signedPdfFilePath = Path.Combine(Environment.CurrentDirectory, "Invoice_signed.pdf");
 pdfDoc.Save(new FileStream(signedPdfFilePath, FileMode.Create));
